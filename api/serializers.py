2327bf5ae8623b8adb66b9ee122ea7836e792050
Nick <nickphilomath@gmail.com>
2024-03-05 13:47:33 +0500

    update ad serializer

--- api/serializers.py
+++ api/serializers.py
@@ -88,5 +88,5 @@ class AdvertisementSerializer(serializers.ModelSerializer):
             'about',
             'price',
             'currency',
-            'is_auto_renew',
+            'date_posted',
         ]
